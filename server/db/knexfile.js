5ca9bae787b9459a6ba161cae262e248c5e1df67
Kevin Qi <iqnivek@gmail.com>
2019-10-29 14:03:24 -0700

    RDS db config

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -14,7 +14,12 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+    },
     migrations: {
       directory: path.join(__dirname, '/migrations'),
       tableName: 'knex_migrations',
